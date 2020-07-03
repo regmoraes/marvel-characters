8a94fbe94ef96498de8d8808a154d6da82d34c69
Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>
2020-07-03 11:31:22 -0300

    Remove unnecessary map
    Signed-off-by: Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>

--- app/src/main/java/br/com/regmoraes/marvelcharacters/application/FetchFavorites.kt
+++ app/src/main/java/br/com/regmoraes/marvelcharacters/application/FetchFavorites.kt
@@ -2,11 +2,9 @@ package br.com.regmoraes.marvelcharacters.application
 
 import br.com.regmoraes.marvelcharacters.infrastructure.CharacterRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-
 
 class FetchFavorites(private val characterRepository: CharacterRepository) {
     fun execute(): Flow<FavoritesEvent> {
-        return characterRepository.getFavorites().map { it }
+        return characterRepository.getFavorites()
     }
 }
